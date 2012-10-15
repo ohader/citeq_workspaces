TYPO3/typo3

[FEATURE] Integrate language handling in workspace module
Integrate language handling in workspace module. First a new
column "language" is integrated to the toolbar section that
shows the available sys_language records of the system. This
column allows to sort by the language uid as well.

Besides that a new virtual column "record status" is itegrated.
This is needed if e.g. a translated record and the default
parent record are new on the current workspace and only the
translated record shall be published - in this case the
localization might not be shown in the frontend since it just
cannot overlay the default record which is not available.
This behaviour is called "integrity check of records".

Change-Id: I89370281f6fdcd5d34e805b2d093a020fbdd4b2a
Resolves: #36790
Releases: 6.0
Reviewed-on: http://review.typo3.org/13902
Reviewed-by: Wouter Wolters
Reviewed-by: Oliver Hader
Tested-by: Oliver Hader

typo3/sysext/workspaces/Classes/ExtDirect/AbstractHandler.php
@@ -72,5 +72,81 @@ abstract class Tx_Workspaces_ExtDirect_AbstractHandler {
 	protected function getWorkspaceService() {
 		return t3lib_div::makeInstance('Tx_Workspaces_Service_Workspaces');
 	}
+
+	/**
+	 * Validates whether the submitted language parameter can be
+	 * interpreted as integer value.
+	 *
+	 * @param stdClass $parameters
+	 * @return integer|NULL
+	 */
+	protected function validateLanguageParameter(stdClass $parameters) {
+		$language = NULL;
+
+		if (isset($parameters->language) && t3lib_utility_Math::canBeInterpretedAsInteger($parameters->language)) {
+			$language = $parameters->language;
+		}
+
+		return $language;
+	}
+
+	/**
+	 * Gets affected elements on publishing/swapping actions.
+	 * Affected elements have a dependency, e.g. translation overlay
+	 * and the default origin record - thus, the default record would be
+	 * affected if the translation overlay shall be published.
+	 *
+	 * @param stdClass $parameters
+	 * @return array
+	 */
+	protected function getAffectedElements(stdClass $parameters) {
+		$affectedElements = array();
+
+		if ($parameters->type === 'selection') {
+			foreach ((array) $parameters->selection as $element) {
+				$affectedElements[] = Tx_Workspaces_Domain_Model_CombinedRecord::create(
+					$element->table,
+					$element->liveId,
+					$element->versionId
+				);
+			}
+		} elseif ($parameters->type === 'all') {
+			$versions = $this->getWorkspaceService()->selectVersionsInWorkspace(
+				$this->getCurrentWorkspace(),
+				0, -99, -1, 0, 'tables_select',
+				$this->validateLanguageParameter($parameters)
+			);
+
+			foreach ($versions as $table => $tableElements) {
+				foreach ($tableElements as $element) {
+					$affectedElement = Tx_Workspaces_Domain_Model_CombinedRecord::create(
+						$table,
+						$element['t3ver_oid'],
+						$element['uid']
+					);
+
+					$affectedElement->getVersionRecord()->setRow($element);
+					$affectedElements[] = $affectedElement;
+				}
+			}
+		}
+
+		return $affectedElements;
+	}
+
+	/**
+	 * Creates a new instance of the integrity service for the
+	 * given set of affected elements.
+	 *
+	 * @param Tx_Workspaces_Domain_Model_CombinedRecord[] $affectedElements
+	 * @return Tx_Workspaces_Service_Integrity
+	 * @see getAffectedElements
+	 */
+	protected function createIntegrityService(array $affectedElements) {
+		/** @var $integrityService Tx_Workspaces_Service_Integrity */
+		$integrityService = t3lib_div::makeInstance('Tx_Workspaces_Service_Integrity');
+		$integrityService->setAffectedElements($affectedElements);
+		return $integrityService;
+	}
 }
 ?>

typo3/sysext/workspaces/Classes/Service/Workspaces.php
@@ -117,9 +117,10 @@ class Tx_Workspaces_Service_Workspaces implements t3lib_Singleton {
 	 * @param	integer		Real workspace ID, cannot be ONLINE (zero).
 	 * @param	boolean		If set, then the currently online versions are swapped into the workspace in exchange for the offline versions. Otherwise the workspace is emptied.
 	 * @param	integer		$pageId: ...
+	 * @param	integer		$language Select specific language only
 	 * @return	array		Command array for tcemain
 	 */
-	public function getCmdArrayForPublishWS($wsid, $doSwap, $pageId = 0) {
+	public function getCmdArrayForPublishWS($wsid, $doSwap, $pageId = 0, $language = NULL) {
 
 		$wsid = intval($wsid);
 		$cmd = array();
@@ -136,7 +137,7 @@ class Tx_Workspaces_Service_Workspaces implements t3lib_Singleton {
 			}
 
 				// Select all versions to swap:
-			$versions = $this->selectVersionsInWorkspace($wsid, 0, $stage, ($pageId ? $pageId : -1), 0, 'tables_modify');
+			$versions = $this->selectVersionsInWorkspace($wsid, 0, $stage, ($pageId ? $pageId : -1), 0, 'tables_modify', $language);
 
 				// Traverse the selection to build CMD array:
 			foreach ($versions as $table => $records) {
@@ -156,9 +157,10 @@ class Tx_Workspaces_Service_Workspaces implements t3lib_Singleton {
 	 * @param	integer		Real workspace ID, cannot be ONLINE (zero).
 	 * @param	boolean		Run Flush (TRUE) or ClearWSID (FALSE) command
 	 * @param	integer		$pageId: ...
+	 * @param	integer		$language Select specific language only
 	 * @return	array		Command array for tcemain
 	 */
-	public function getCmdArrayForFlushWS($wsid, $flush = TRUE, $pageId = 0) {
+	public function getCmdArrayForFlushWS($wsid, $flush = TRUE, $pageId = 0, $language = NULL) {
 
 		$wsid = intval($wsid);
 		$cmd = array();
@@ -168,7 +170,7 @@ class Tx_Workspaces_Service_Workspaces implements t3lib_Singleton {
 			$stage = -99;
 
 				// Select all versions to swap:
-			$versions = $this->selectVersionsInWorkspace($wsid, 0, $stage, ($pageId ? $pageId : -1), 0, 'tables_modify');
+			$versions = $this->selectVersionsInWorkspace($wsid, 0, $stage, ($pageId ? $pageId : -1), 0, 'tables_modify', $language);
 
 				// Traverse the selection to build CMD array:
 			foreach ($versions as $table => $records) {
@@ -193,9 +195,10 @@ class Tx_Workspaces_Service_Workspaces implements t3lib_Singleton {
 	 * @param	integer		Page id: Live page for which to find versions in workspace!
 	 * @param	integer		Recursion Level - select versions recursive - parameter is only relevant if $pageId != -1
 	 * @param	string		How to collect records for "listing" or "modify" these tables. Support the permissions of each type of record (@see t3lib_userAuthGroup::check).
+	 * @parem	integer		$language Select specific language only
 	 * @return	array		Array of all records uids etc. First key is table name, second key incremental integer. Records are associative arrays with uid and t3ver_oidfields. The pid of the online record is found as "livepid" the pid of the offline record is found in "wspid"
 	 */
-	public function selectVersionsInWorkspace($wsid, $filter = 0, $stage = -99, $pageId = -1, $recursionLevel = 0, $selectionType = 'tables_select') {
+	public function selectVersionsInWorkspace($wsid, $filter = 0, $stage = -99, $pageId = -1, $recursionLevel = 0, $selectionType = 'tables_select', $language = NULL) {
 
 		$wsid = intval($wsid);
 		$filter = intval($filter);
@@ -231,7 +234,7 @@ class Tx_Workspaces_Service_Workspaces implements t3lib_Singleton {
 
 			if ($GLOBALS['TCA'][$table]['ctrl']['versioningWS']) {
 
-				$recs = $this->selectAllVersionsFromPages($table, $pageList, $wsid, $filter, $stage);
+				$recs = $this->selectAllVersionsFromPages($table, $pageList, $wsid, $filter, $stage, $language);
 				if (intval($GLOBALS['TCA'][$table]['ctrl']['versioningWS']) === 2) {
 					$moveRecs = $this->getMoveToPlaceHolderFromPages($table, $pageList, $wsid, $filter, $stage);
 					$recs = array_merge($recs, $moveRecs);
@@ -253,22 +256,40 @@ class Tx_Workspaces_Service_Workspaces implements t3lib_Singleton {
 	 * @param integer $wsid
 	 * @param integer $filter
 	 * @param integer $stage
+	 * @param integer $language
 	 * @return array
 	 */
-	protected function selectAllVersionsFromPages($table, $pageList, $wsid, $filter, $stage) {
+	protected function selectAllVersionsFromPages($table, $pageList, $wsid, $filter, $stage, $language = NULL) {
+		$isTableLocalizable = t3lib_BEfunc::isTableLocalizable($table);
+		$languageParentField = '';
 
-		$fields = 'A.uid, A.t3ver_oid, A.t3ver_stage, B.pid AS wspid, B.pid AS livepid';
-		if (t3lib_BEfunc::isTableLocalizable($table)) {
+			// If table is not localizable, but localized reocrds shall
+			// be collected, an empty result array needs to be returned:
+		if ($isTableLocalizable === FALSE && $language > 0) {
+			return array();
+		} elseif ($isTableLocalizable) {
+			$languageParentField = 'A.' . $GLOBALS['TCA'][$table]['ctrl']['transOrigPointerField'] . ', ';
+		}
+
+		$fields = 'A.uid, A.t3ver_oid, A.t3ver_stage, ' . $languageParentField . 'B.pid AS wspid, B.pid AS livepid';
+
+		if ($isTableLocalizable) {
 			$fields .= ', A.' . $GLOBALS['TCA'][$table]['ctrl']['languageField'];
 		}
+
 		$from = $table . ' A,' . $table . ' B';
 
 			// Table A is the offline version and pid=-1 defines offline
 		$where = 'A.pid=-1 AND A.t3ver_state!=4';
+
 		if ($pageList) {
 			$pidField = ($table==='pages' ? 'uid' : 'pid');
 			$pidConstraint = strstr($pageList, ',') ? ' IN (' . $pageList . ')' : '=' . $pageList;
 			$where .= ' AND B.' . $pidField . $pidConstraint;
+		}
+
+		if ($isTableLocalizable && t3lib_utility_Math::canBeInterpretedAsInteger($language)) {
+			$where .= ' AND A.' . $GLOBALS['TCA'][$table]['ctrl']['languageField'] . '=' . $language;
 		}
 
 		/**
@@ -382,8 +403,10 @@ class Tx_Workspaces_Service_Workspaces implements t3lib_Singleton {
 		 * mount points are not covered yet
 		 **/
 		$perms_clause = $GLOBALS['BE_USER']->getPagePermsClause(1);
+
+		/** @var $searchObj t3lib_fullsearch */
 		$searchObj = t3lib_div::makeInstance('t3lib_fullsearch');
-		$pageList = FALSE;
+
 		if ($pageId > 0) {
 			$pageList = $searchObj->getTreeList($pageId, $recursionLevel, 0, $perms_clause);
 		} else {

ohader/citeq_workspaces

Conflicts:
	Classes/ExtDirect/AbstractHandler.php
	Classes/ExtDirect/MassActionHandler.php
	Classes/ExtDirect/Server.php
	Classes/Service/Befunc.php
	Classes/Service/GridData.php
	Classes/Service/Workspaces.php
	Resources/Private/Language/locallang.xlf
	Resources/Public/JavaScript/Store/mainstore.js
	Resources/Public/JavaScript/configuration.js
	Resources/Public/JavaScript/grid.js
	Resources/Public/JavaScript/toolbar.js

Classes/ExtDirect/AbstractHandler.php
@@ -66,8 +66,92 @@ abstract class tx_Workspaces_ExtDirect_AbstractHandler {
 
 		return $response;
 	}
-}
 
+	/**
+	 * Gets an instance of the workspaces service.
+	 *
+	 * @return Tx_Workspaces_Service_Workspaces
+	 */
+	protected function getWorkspaceService() {
+		return t3lib_div::makeInstance('Tx_Workspaces_Service_Workspaces');
+	}
+
+	/**
+	 * Validates whether the submitted language parameter can be
+	 * interpreted as integer value.
+	 *
+	 * @param stdClass $parameters
+	 * @return integer|NULL
+	 */
+	protected function validateLanguageParameter(stdClass $parameters) {
+		$language = NULL;
+
+		if (isset($parameters->language) && t3lib_div::testInt($parameters->language)) {
+			$language = $parameters->language;
+		}
+
+		return $language;
+	}
+
+	/**
+	 * Gets affected elements on publishing/swapping actions.
+	 * Affected elements have a dependency, e.g. translation overlay
+	 * and the default origin record - thus, the default record would be
+	 * affected if the translation overlay shall be published.
+	 *
+	 * @param stdClass $parameters
+	 * @return array
+	 */
+	protected function getAffectedElements(stdClass $parameters) {
+		$affectedElements = array();
+
+		if ($parameters->type === 'selection') {
+			foreach ((array) $parameters->selection as $element) {
+				$affectedElements[] = Tx_Workspaces_Domain_Model_CombinedRecord::create(
+					$element->table,
+					$element->liveId,
+					$element->versionId
+				);
+			}
+		} elseif ($parameters->type === 'all') {
+			$versions = $this->getWorkspaceService()->selectVersionsInWorkspace(
+				$this->getCurrentWorkspace(),
+				0, -99, -1, 0, 'tables_select',
+				$this->validateLanguageParameter($parameters)
+			);
+
+			foreach ($versions as $table => $tableElements) {
+				foreach ($tableElements as $element) {
+					$affectedElement = Tx_Workspaces_Domain_Model_CombinedRecord::create(
+						$table,
+						$element['t3ver_oid'],
+						$element['uid']
+					);
+
+					$affectedElement->getVersionRecord()->setRow($element);
+					$affectedElements[] = $affectedElement;
+				}
+			}
+		}
+
+		return $affectedElements;
+	}
+
+	/**
+	 * Creates a new instance of the integrity service for the
+	 * given set of affected elements.
+	 *
+	 * @param Tx_Workspaces_Domain_Model_CombinedRecord[] $affectedElements
+	 * @return Tx_Workspaces_Service_Integrity
+	 * @see getAffectedElements
+	 */
+	protected function createIntegrityService(array $affectedElements) {
+		/** @var $integrityService Tx_Workspaces_Service_Integrity */
+		$integrityService = t3lib_div::makeInstance('Tx_Workspaces_Service_Integrity');
+		$integrityService->setAffectedElements($affectedElements);
+		return $integrityService;
+	}
+}
 
 if (defined('TYPO3_MODE') && isset($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/workspaces/Classes/ExtDirect/AbstractHandler.php'])) {
 	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/workspaces/Classes/ExtDirect/AbstractHandler.php']);

Classes/Service/Workspaces.php
@@ -104,9 +104,10 @@ class tx_Workspaces_Service_Workspaces {
 	 * @param	integer		Real workspace ID, cannot be ONLINE (zero).
 	 * @param	boolean		If set, then the currently online versions are swapped into the workspace in exchange for the offline versions. Otherwise the workspace is emptied.
 	 * @param	integer		$pageId: ...
+	 * @param	integer		$language Select specific language only
 	 * @return	array		Command array for tcemain
 	 */
-	public function getCmdArrayForPublishWS($wsid, $doSwap, $pageId = 0) {
+	public function getCmdArrayForPublishWS($wsid, $doSwap, $pageId = 0, $language = NULL) {
 
 		$wsid = intval($wsid);
 		$cmd = array();
@@ -123,7 +124,7 @@ class tx_Workspaces_Service_Workspaces {
 			}
 
 				// Select all versions to swap:
-			$versions = $this->selectVersionsInWorkspace($wsid, 0, $stage, ($pageId ? $pageId : -1), 0, 'tables_modify');
+			$versions = $this->selectVersionsInWorkspace($wsid, 0, $stage, ($pageId ? $pageId : -1), 0, 'tables_modify', $language);
 
 				// Traverse the selection to build CMD array:
 			foreach ($versions as $table => $records) {
@@ -143,9 +144,10 @@ class tx_Workspaces_Service_Workspaces {
 	 * @param	integer		Real workspace ID, cannot be ONLINE (zero).
 	 * @param	boolean		Run Flush (true) or ClearWSID (false) command
 	 * @param	integer		$pageId: ...
+	 * @param	integer		$language Select specific language only
 	 * @return	array		Command array for tcemain
 	 */
-	public function getCmdArrayForFlushWS($wsid, $flush = TRUE, $pageId = 0) {
+	public function getCmdArrayForFlushWS($wsid, $flush = TRUE, $pageId = 0, $language = NULL) {
 
 		$wsid = intval($wsid);
 		$cmd = array();
@@ -155,7 +157,7 @@ class tx_Workspaces_Service_Workspaces {
 			$stage = -99;
 
 				// Select all versions to swap:
-			$versions = $this->selectVersionsInWorkspace($wsid, 0, $stage, ($pageId ? $pageId : -1), 0, 'tables_modify');
+			$versions = $this->selectVersionsInWorkspace($wsid, 0, $stage, ($pageId ? $pageId : -1), 0, 'tables_modify', $language);
 
 				// Traverse the selection to build CMD array:
 			foreach ($versions as $table => $records) {
@@ -180,9 +182,10 @@ class tx_Workspaces_Service_Workspaces {
 	 * @param	integer		Page id: Live page for which to find versions in workspace!
 	 * @param	integer		Recursion Level - select versions recursive - parameter is only relevant if $pageId != -1
 	 * @param	string		How to collect records for "listing" or "modify" these tables. Support the permissions of each type of record (@see t3lib_userAuthGroup::check).
-	 * @return	array		Array of all records uids etc. First key is table name, second key incremental integer. Records are associative arrays with uid, t3ver_oid and t3ver_swapmode fields. The pid of the online record is found as "livepid" the pid of the offline record is found in "wspid"
+	 * @parem	integer		$language Select specific language only
+	 * @return	array		Array of all records uids etc. First key is table name, second key incremental integer. Records are associative arrays with uid and t3ver_oidfields. The pid of the online record is found as "livepid" the pid of the offline record is found in "wspid"
 	 */
-	public function selectVersionsInWorkspace($wsid, $filter = 0, $stage = -99, $pageId = -1, $recursionLevel = 0, $selectionType = 'tables_select') {
+	public function selectVersionsInWorkspace($wsid, $filter = 0, $stage = -99, $pageId = -1, $recursionLevel = 0, $selectionType = 'tables_select', $language = NULL) {
 
 		$wsid = intval($wsid);
 		$filter = intval($filter);
@@ -207,7 +210,7 @@ class tx_Workspaces_Service_Workspaces {
 
 			if ($GLOBALS['TCA'][$table]['ctrl']['versioningWS']) {
 
-				$recs = $this->selectAllVersionsFromPages($table, $pageList, $wsid, $filter, $stage);
+				$recs = $this->selectAllVersionsFromPages($table, $pageList, $wsid, $filter, $stage, $language);
 				if (intval($GLOBALS['TCA'][$table]['ctrl']['versioningWS']) === 2) {
 					$moveRecs = $this->getMoveToPlaceHolderFromPages($table, $pageList, $wsid, $filter, $stage);
 					$recs = array_merge($recs, $moveRecs);
@@ -228,22 +231,40 @@ class tx_Workspaces_Service_Workspaces {
 	 * @param string $pageList
 	 * @param integer $filter
 	 * @param integer $stage
+	 * @param integer $language
 	 * @return array
 	 */
-	protected function selectAllVersionsFromPages($table, $pageList, $wsid, $filter, $stage) {
+	protected function selectAllVersionsFromPages($table, $pageList, $wsid, $filter, $stage, $language = NULL) {
+		$isTableLocalizable = t3lib_BEfunc::isTableLocalizable($table);
+		$languageParentField = '';
 
-		$fields = 'A.uid, A.t3ver_oid,' . ($table==='pages' ? ' A.t3ver_swapmode,' : '') . 'B.pid AS wspid, B.pid AS livepid';
-		if (t3lib_BEfunc::isTableLocalizable($table)) {
+			// If table is not localizable, but localized reocrds shall
+			// be collected, an empty result array needs to be returned:
+		if ($isTableLocalizable === FALSE && $language > 0) {
+			return array();
+		} elseif ($isTableLocalizable) {
+			$languageParentField = 'A.' . $GLOBALS['TCA'][$table]['ctrl']['transOrigPointerField'] . ', ';
+		}
+
+		$fields = 'A.uid, A.t3ver_oid, A.t3ver_stage, ' . $languageParentField . 'B.pid AS wspid, B.pid AS livepid';
+
+		if ($isTableLocalizable) {
 			$fields .= ', A.' . $GLOBALS['TCA'][$table]['ctrl']['languageField'];
 		}
+
 		$from = $table . ' A,' . $table . ' B';
 
 			// Table A is the offline version and pid=-1 defines offline
 		$where = 'A.pid=-1 AND A.t3ver_state!=4';
+
 		if ($pageList) {
 			$pidField = ($table==='pages' ? 'uid' : 'pid');
 			$pidConstraint = strstr($pageList, ',') ? ' IN (' . $pageList . ')' : '=' . $pageList;
 			$where .= ' AND B.' . $pidField . $pidConstraint;
+		}
+
+		if ($isTableLocalizable && t3lib_div::testInt($language)) {
+			$where .= ' AND A.' . $GLOBALS['TCA'][$table]['ctrl']['languageField'] . '=' . $language;
 		}
 
 		/**
@@ -355,8 +376,10 @@ class tx_Workspaces_Service_Workspaces {
 		 * mount points are not covered yet
 		 **/
 		$perms_clause = $GLOBALS['BE_USER']->getPagePermsClause(1);
+
+		/** @var $searchObj t3lib_fullsearch */
 		$searchObj = t3lib_div::makeInstance('t3lib_fullsearch');
-		$pageList = FALSE;
+
 		if ($pageId > 0) {
 			$pageList = $searchObj->getTreeList($pageId, $recursionLevel, 0, $perms_clause);
 		} else {